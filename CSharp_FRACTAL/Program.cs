2b62baa3fc6ca5eaf3242542c174a195c2d5b554
tqaw19 <tqaw19@gmail.com>
2025-11-11 22:10:22 -0500

    Diccionarios

--- CSharp_FRACTAL/Program.cs
+++ CSharp_FRACTAL/Program.cs
@@ -6,11 +6,31 @@ namespace CSharp_FRACTAL
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
-            foreach (int number in numbers)
+            string[] frutas = new string[3];
+            frutas[0] = "Manzana";
+            frutas[1] = "Banana";
+            frutas[2] = "Naranja";
+
+            foreach (string fruta in frutas)
             {
-                Console.WriteLine(number);
+                Console.WriteLine(fruta);
+            }
+
+            var frutasList = new List<string> { "Manzana", "Banana", "Naranja" };
+            foreach (string fruta in frutasList)
+            {
+                Console.WriteLine(fruta);
+            }
+
+            Dictionary<string, int> frutasDict = new Dictionary<string, int>();
+            frutasDict.Add("Manzana", 1);
+            frutasDict.Add("Banana", 2);
+            frutasDict.Add("Naranja", 3);
+            frutasDict.Add("Pina", 3);
+
+            foreach (var fruta in frutasDict)
+            {
+                Console.WriteLine($"Fruta: {fruta.Key} - {fruta.Value}");
             }
         }
     }
