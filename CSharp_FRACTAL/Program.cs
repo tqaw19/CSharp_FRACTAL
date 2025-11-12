44e4ebf5bd4d8d5065c53d59c5e6d89c832a4996
tqaw19 <tqaw19@gmail.com>
2025-11-11 21:03:53 -0500

    first commit!

---
+++ CSharp_FRACTAL/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace CSharp_FRACTAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
